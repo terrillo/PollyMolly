850335e9d89419f149b6f8aa580bd0c36e6939a2
Terrillo Walls <terrillo@terrillo.me>
2017-06-24 08:53:48 -0500

    Example

---
+++ example/index.js
@@ -0,0 +1,12 @@
+const PollyMolly = require('pollymolly')
+
+// Text -> SSML
+console.log(PollyMolly.text2ssml('Hello World'))
+
+// SSML -> Text
+console.log(PollyMolly.ssml2text('<speak>Hello World</speak>'))
+
+// Download File
+PollyMolly.download('Hello World', 'media/hello.mp3', function() {
+  console.log('File Saved!')
+})
